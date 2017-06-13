0c6ef518805538be8ff73cb0dc7207d34273ee9b
lemonjai <leocheung_1986@hotmail.com>
2017-06-12 21:30:07 -0400

    backend complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613000630) do
+ActiveRecord::Schema.define(version: 20170613011453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170613000630) do
     t.boolean "is_published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "order"
   end
 
   create_table "pages", force: :cascade do |t|
