d3b52a50a486c1c06808705f138a977ed8e3a144
lemonjai <leocheung_1986@hotmail.com>
2017-06-12 20:21:19 -0400

    this is a test

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'pages/show'
+
+  get 'home/show'
+
+  resources :pages, only: [:show]
+
+  root 'home#show'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
