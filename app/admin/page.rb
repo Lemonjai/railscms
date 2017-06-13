98bc6db73053c4e9bf339b683f120bcbe698c5d6
lemonjai <leocheung_1986@hotmail.com>
2017-06-12 22:10:26 -0400

    add feature pages

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Page do
 
-permit_params :title, :section, :body, :order, :is_published, :menu_display, :section_id
+permit_params :title, :section, :body, :order, :is_published, :menu_display, :section_id, :featured
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -32,6 +32,7 @@ permit_params :title, :section, :body, :order, :is_published, :menu_display, :se
       f.input :body, as: :html_editor, :label => "Body"
       f.input :order, :label => "Order"
       f.input :is_published, :label => "Published"
+      f.input :featured, :label => "Featured"
       f.input :menu_display, :label => "Display In Menu"
       f.actions
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613011453) do
+ActiveRecord::Schema.define(version: 20170613015758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20170613011453) do
     t.boolean "menu_display"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "featured"
   end
 
   create_table "sections", force: :cascade do |t|
